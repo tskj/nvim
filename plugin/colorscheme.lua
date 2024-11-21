cc9bd66120bdf2ecd3ecc0a28de4d0887af3a1e8
GitHub Action <action@github.com>
2024-11-21 14:13:10 +0000

    Update Lua files - 71acdfc0d3a07ddf6ffb15798902d71eff34f4fb

--- plugin/colorscheme.lua
+++ plugin/colorscheme.lua
@@ -5,4 +5,4 @@ end
 math.randomseed(os.time())
 local nightfox = require("nightfox")
 nightfox.setup({options = {dim_inactive = true}})
-return vim.cmd(("colorscheme " .. pick_random({"nightfox", "duskfox", "nordfox", "terafox", "carbonfox"})))
+return vim.cmd(("colorscheme " .. pick_random({"nightfox", "duskfox", "terafox", "carbonfox", "torte"})))
--- plugin/configs/tree-sitter.lua
+++ plugin/configs/tree-sitter.lua
@@ -5,4 +5,4 @@ do
   local setup = _let_2_["setup"]
   setup({highlight = {enable = true, additional_vim_regex_highlighting = false}, ensure_installed = {"typescript", "fennel"}, indent = {enable = true}, textobjects = {move = {enable = true, goto_next_start = {["]]"] = "@block.outer"}, goto_next_end = {["]["] = "@block.outer"}, goto_previous_start = {["[["] = "@block.outer"}, goto_previous_end = {["[]"] = "@block.outer"}}, select = {enable = true, keymaps = {ab = "@block.outer", ib = "@block.inner"}, lookahead = true}}})
 end
-return run(require("treesitter-context").setup, {mode = "topline"})
+return run(require("treesitter-context").setup, {mode = "topline", line_numbers = true, multiline_threshold = 1, max_lines = 1, trim_scope = "inner"})
