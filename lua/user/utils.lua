a0a6b6cd0c845328b0e77f4e51671f04f4e3e3f5
GitHub Action <action@github.com>
2025-03-09 20:03:50 +0000

    Update Lua files - 359fe7a61e7e474ad49220e54221f364b4cc2219

--- lua/user/utils.lua
+++ lua/user/utils.lua
@@ -9,17 +9,17 @@ local function debug_log(...)
   local messages
   local _1_
   do
-    local tbl_21_auto = {}
-    local i_22_auto = 0
+    local tbl_21_ = {}
+    local i_22_ = 0
     for __4_auto, x_5_auto in ipairs(msgs) do
-      local val_23_auto = tostring(x_5_auto)
-      if (nil ~= val_23_auto) then
-        i_22_auto = (i_22_auto + 1)
-        tbl_21_auto[i_22_auto] = val_23_auto
+      local val_23_ = tostring(x_5_auto)
+      if (nil ~= val_23_) then
+        i_22_ = (i_22_ + 1)
+        tbl_21_[i_22_] = val_23_
       else
       end
     end
-    _1_ = tbl_21_auto
+    _1_ = tbl_21_
   end
   messages = table.concat(_1_, " ")
   local log_line = (os.date("[%Y-%m-%dT%H:%M:%S] ") .. messages .. "\n")
