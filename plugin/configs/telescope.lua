bfa1182e802f865472e42e9007804ecafe0fe1b7
GitHub Action <action@github.com>
2025-06-19 08:33:23 +0000

    Update Lua files - f9df2b4be1e1ffa0199e10095c502b0e37b28586

--- plugin/configs/telescope.lua
+++ plugin/configs/telescope.lua
@@ -2,7 +2,7 @@ local _local_1_ = require("user.utils")
 local run = _local_1_["run"]
 local actions = require("telescope.actions")
 local telescope = require("telescope")
-telescope.setup({extensions = {["ui-select"] = {run(require("telescope.themes").get_dropdown)}, undo = {}}, defaults = {layout_strategy = "flex", layout_config = {flip_columns = 120, horizontal = {width = 0.9, preview_width = 0.6, prompt_position = "top"}, vertical = {prompt_position = "top", mirror = true}}, sorting_strategy = "ascending", scroll_strategy = "limit", mappings = {i = {["<C-x>"] = actions.delete_buffer}, n = {["<C-x>"] = actions.delete_buffer}}}})
+telescope.setup({extensions = {["ui-select"] = {run(require("telescope.themes").get_dropdown)}, undo = {}}, defaults = {layout_strategy = "flex", layout_config = {horizontal = {width = 0.9, preview_width = 0.6, prompt_position = "top"}, vertical = {prompt_position = "top", mirror = true}, flex = {flip_columns = 120}}, sorting_strategy = "ascending", scroll_strategy = "limit", mappings = {i = {["<C-x>"] = actions.delete_buffer}, n = {["<C-x>"] = actions.delete_buffer}}}})
 pcall(require("telescope").load_extension, "fzf")
 pcall(require("telescope").load_extension, "ui-selet")
 telescope.load_extension("ui-select")
